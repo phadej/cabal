b095de7d4cc1d7bb88f2cf1a424185d784759245
Matthew Pickering <matthewtpickering@gmail.com>
2025-10-06 12:49:01 +0100

    fix

--- Cabal-syntax/src/Distribution/Types/InstalledPackageInfo.hs
+++ Cabal-syntax/src/Distribution/Types/InstalledPackageInfo.hs
@@ -161,6 +161,7 @@ emptyInstalledPackageInfo =
     , hsLibraries = []
     , extraLibraries = []
     , extraLibrariesStatic = []
+    , libraryBytecodeDirs = []
     , extraGHCiLibraries = []
     , includeDirs = []
     , includes = []
